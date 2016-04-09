0a9a8a3bd7693c57f30c10fc674e662e43ca7636
Germán Molina <gmc.ykw@gmail.com>
2016-04-09 21:12:22 +0200

    nueva url y view para aniadir un item a una lista existente. FT passes :-)

--- list/views.py
+++ list/views.py
@@ -9,10 +9,10 @@ def home_page(request):
     return render(request, 'list/home.html')
 
 
-def view_list(request):
-    items = Item.objects.all()
+def view_list(request, list_id):
+    list_ = List.objects.get(id=list_id)
     return render(request, 'list/list.html', {
-        'items': items
+        'list': list_
     })
 
 
@@ -20,4 +20,11 @@ def new_list(request):
     list_ = List.objects.create()
     new_item_text = request.POST.get('item_text', '')
     Item.objects.create(text=new_item_text, list=list_)
-    return redirect('/lists/the-only-list-in-the-world/')
+    return redirect('/lists/%d/' % (list_.id,))
+
+
+def add_item(request, list_id):
+    list_ = List.objects.get(id=list_id)
+    new_item_text = request.POST.get('item_text', '')
+    Item.objects.create(text=new_item_text, list=list_)
+    return redirect('/lists/%d/' % (list_.id,))
