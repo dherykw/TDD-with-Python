242a4e2522cbdf47650fcbb9c8d0e6d83867cfe7
Germán Molina <gmc.ykw@gmail.com>
2016-05-14 20:08:08 +0200

    Adjust new list view to do model validation

--- funcional_tests/test_list_item_validation.py
+++ funcional_tests/test_list_item_validation.py
@@ -12,8 +12,8 @@ class TestNewVisitor(FunctionalTest):
         self.browser.find_element_by_id('id_new_item').send_keys('\n')
 
         # La home se refresca y aparece un error diciendo que la lista no puede contener item vacíos
-        error = self.browser.find_element_by_css_selector('.has_error')
-        self.assertEqual(error.text, "You can't have any empty list item ")
+        error = self.browser.find_element_by_css_selector(".has-error")
+        self.assertEqual(error.text, "You can't have any empty list item")
 
         # Pepe intenta otra ver introducit un item, esta vez con texto y funciona.
         self.browser.find_element_by_id('id_new_item').send_keys('Buy milk\n')
@@ -24,8 +24,8 @@ class TestNewVisitor(FunctionalTest):
 
         # vuelve a recibir el mensaje de error de la página.
         self.check_for_row_in_list_table('1. Buy milk')
-        error = self.browser.find_element_by_css_selector('.has_error')
-        self.assertEqual(error.text, "You can't have any empty list item ")
+        error = self.browser.find_element_by_css_selector(".has-error")
+        self.assertEqual(error.text, "You can't have any empty list item")
 
         # lo corrige e introduce texto
         self.browser.find_element_by_id('id_new_item').send_keys('Make tea\n')
--- list/views.py
+++ list/views.py
@@ -1,3 +1,4 @@
+from django.core.exceptions import ValidationError
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from list.models import *
@@ -19,7 +20,14 @@ def view_list(request, list_id):
 def new_list(request):
     list_ = List.objects.create()
     new_item_text = request.POST.get('item_text', '')
-    Item.objects.create(text=new_item_text, list=list_)
+    item = Item(text=new_item_text, list=list_)
+    try:
+        item.full_clean()
+        item.save()
+    except ValidationError:
+        list_.delete()
+        error = "You can't have any empty list item"
+        return render(request, 'list/home.html', {"error": error})
     return redirect('/lists/%d/' % (list_.id,))
 
 
