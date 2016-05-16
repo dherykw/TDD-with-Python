8467648ded0d89e42a742599fb0376173a8745ea
Germán Molina <gmc.ykw@gmail.com>
2016-05-17 01:06:55 +0200

    Refactor hard-coded URLs out of templates and list view add item bug fixed

--- list/views.py
+++ list/views.py
@@ -1,10 +1,8 @@
 from django.core.exceptions import ValidationError
 from django.shortcuts import render, redirect
-from django.http import HttpResponse
+
 from list.models import *
 
-
-# Create your views here.
 
 def home_page(request):
     return render(request, 'list/home.html')
@@ -12,13 +10,18 @@ def home_page(request):
 
 def view_list(request, list_id):
     list_ = List.objects.get(id=list_id)
+    error = None
 
     if request.method == 'POST':
-        new_item_text = request.POST['item_text']
-        Item.objects.create(text=new_item_text, list=list_)
-        return redirect('/lists/%d/' % list_.id)
+        try:
+            new_item_text = request.POST.get('item_text', '')
+            item = Item(text=new_item_text, list=list_)
+            item.full_clean()
+            item.save()
+        except ValidationError:
+            error = "You can't have any empty list item"
 
-    return render(request, 'list/list.html', {'list': list_})
+    return render(request, 'list/list.html', {'list': list_, 'error': error})
 
 
 def new_list(request):
@@ -31,6 +34,5 @@ def new_list(request):
     except ValidationError:
         list_.delete()
         error = "You can't have any empty list item"
-        return render(request, 'list/home.html', {"error": error})
+        return render(request, 'list/home.html', {'error': error})
     return redirect('/lists/%d/' % (list_.id,))
-
